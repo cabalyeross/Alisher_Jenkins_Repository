7366a66dbbc0ad386966f2c2c5939429a69d557a
Alisher Khidirov <77864980+cabalyeross@users.noreply.github.com>
2021-02-28 15:04:15 -0500

    second change

--- src/main/java/utils/WebdriverUtils.java
+++ src/main/java/utils/WebdriverUtils.java
@@ -3,11 +3,18 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class WebdriverUtils {
 
+    private static String sauceUser = LocalConfigUtils.getProperty("sauceUser");
+    private static String sauceKey = LocalConfigUtils.getProperty("sauceKey");
+    private static String URL = "https://" + sauceUser + ":" + sauceKey + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
 
     //SINGLETON WEBDRIVER
 
@@ -19,26 +26,27 @@ public class WebdriverUtils {
 
 
     public static WebDriver getWebDriver(){
-        if(driver==null){
-            String browserType = LocalConfigUtils.getProperty("browser");
-            System.out.println("broswer::: " +browserType);
-
-            switch (browserType.toLowerCase()){
-                case "firefox" :
-                    System.setProperty("webdriver.gecko.driver" , "/Users/alisherkhidirov/Projects/selenium client web driver /geckodriver");
-                    driver = new FirefoxDriver();
-                    break;
-                default:
-                    System.setProperty("webdriver.chrome.driver" , "/Users/alisherkhidirov/Projects/selenium client web driver /chromedriver");
-                    driver = new ChromeDriver();
-                    break;
+        if(driver==null) {
+            if (LocalConfigUtils.getProperty("runInSaucelabs").equalsIgnoreCase("true")) {
+                driver = getRemoteDriver();
+            } else {
+                String browserType = LocalConfigUtils.getProperty("browser");
+                System.out.println("broswer::: " + browserType);
+                switch (browserType.toLowerCase()) {
+                    case "firefox":
+                        System.setProperty("webdriver.gecko.driver", "" +
+                                "");
+                        driver = new FirefoxDriver();
+                        break;
+                    default:
+                        System.setProperty("webdriver.chrome.driver", "/Users/alisherkhidirov/Projects/selenium client web driver /chromedriver");
+                        driver = new ChromeDriver();
+                        break;
+                }
             }
-
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         }
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         return driver;
-
     }
 
 
@@ -51,7 +59,19 @@ public class WebdriverUtils {
         }
     }
 
-
+    public static WebDriver getRemoteDriver(){
+        WebDriver driver = null;
+        try {
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability("browserName","chrome");
+            capabilities.setCapability("version", LocalConfigUtils.getProperty("browser_version"));
+            capabilities.setCapability("platform", LocalConfigUtils.getProperty("os"));
+            driver = new RemoteWebDriver(new URL(URL), capabilities);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return driver;
+    }
 
 
 }
